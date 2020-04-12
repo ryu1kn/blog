c79c96f850382338a46bf727831400f15b692ecc
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-12 22:21:31 +1000

    Write article in markdown

--- src/blog/core.clj
+++ src/blog/core.clj
@@ -1,7 +1,8 @@
 (ns blog.core
-  (:require [blog.blogger :refer [post-article]])
+  (:require [blog.blogger :refer [post-article]]
+            [blog.markdown :refer [render-html]])
   (:gen-class))
 
 (defn -main [& _]
   (post-article "https://blog.ryuichi.io" {:title "My Test Post 2"
-                                           :content "With <b>exciting</b> content!!"}))
+                                           :content (render-html "With *exciting* content!!")}))
